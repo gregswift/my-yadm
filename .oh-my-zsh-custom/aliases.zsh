6487b234aef5df59a6f9c29deaedba9dae63c887
Greg Swift <gregswift@gmail.com>
2023-08-24 10:41:38 -0500

    Revert "Add some variables for managing AWS legacy env variables cause of terraform"
    Because granted.dev handles it better
    
    This reverts commit d2405ca11e4f5957dde5a25752299860a3ff9d05.

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,8 +1,3 @@
 alias docker="podman"
 alias git-rebase='git rebase -i $(git merge-base main $(git branch --show-current))'
 alias jq="docker run --rm -i apteno/alpine-jq jq"
-
-# AWS creds for terraform
-alias tfaws='eval $(aws configure export-credentials --format env --profile iat)'
-# Clear AWS creds
-alias unsetaws='unset AWS_ACCESS_KEY_ID; unset AWS_SECRET_ACCESS_KEY; unset AWS_SESSION_TOKEN; unset AWS_CREDENTIAL_EXPIRATION'
