da1b7305fc0bfe8ef94b88970648d760f3b46f48
Greg Swift <gregswift@gmail.com>
2023-08-24 10:39:21 -0500

    Add support for granted.dev cli tool for AWS profile management

---
+++ .oh-my-zsh-custom/granted.zsh
@@ -0,0 +1,7 @@
+alias assume="source /opt/homebrew/bin/assume"
+
+export GRANTED_ENABLE_AUTO_REASSUME=true
+
+fpath=(/Users/greg.swift/.granted/zsh_autocomplete/assume/ $fpath)
+
+fpath=(/Users/greg.swift/.granted/zsh_autocomplete/granted/ $fpath)
