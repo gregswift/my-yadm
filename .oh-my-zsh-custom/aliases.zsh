5be0772ca71baf58bfed91fe0e0b405f04ca6ab7
Greg Swift <gregswift@gmail.com>
2025-10-05 15:51:34 -0500

    feat: support inferring the default branch of a repo

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,8 +1,10 @@
 alias docker="podman"
 alias git-rebase='git-refresh && git rebase -i $(git merge-base $(git branch -r | grep -Po "HEAD -> \K.*$") $(git branch --show-current))'
-alias git-refresh='git fetch --all -p && git rebase origin/main'
+alias git-refresh='git fetch --all -p && git rebase $(git branch -r | grep -Po "HEAD -> \K.*$")'
 alias jq="docker run --rm -i apteno/alpine-jq jq"
 
 git-rebase-branch () {
-  git-refresh && git-rebase -i $(git merge-base ${1} $(git branch --show-current))
+  git fetch --all -p
+  git rebase ${1}
+  git-rebase -i $(git merge-base ${1} $(git branch --show-current))
 }
