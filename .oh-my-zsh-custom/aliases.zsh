95a42b54afd5b257ae96d094259b0c0120b4dcb5
Greg Swift <gregswift@gmail.com>
2025-10-05 15:28:59 -0500

    feat: add oh-my-zsh custom bits

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,3 +1,8 @@
 alias docker="podman"
-alias git-rebase='git rebase -i $(git merge-base main $(git branch --show-current))'
+alias git-rebase='git-refresh && git rebase -i $(git merge-base $(git branch -r | grep -Po "HEAD -> \K.*$") $(git branch --show-current))'
+alias git-refresh='git fetch --all -p && git rebase origin/main'
 alias jq="docker run --rm -i apteno/alpine-jq jq"
+
+git-rebase-branch () {
+  git-refresh && git-rebase -i $(git merge-base ${1} $(git branch --show-current))
+}
